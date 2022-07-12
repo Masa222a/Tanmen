8d60bb565aa892b876e4a7e65f4a9cec9e363ffb
Masa222a <guitarstring0716@gmail.com>
2022-07-12 13:18:48 +0900

    位置情報取得

--- app/src/main/java/com/android/example/tanmen/Controller/Activity/MainActivity.kt
+++ app/src/main/java/com/android/example/tanmen/Controller/Activity/MainActivity.kt
@@ -1,16 +1,50 @@
 package com.android.example.tanmen.Controller.Activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.android.example.tanmen.Controller.Fragment.SearchBottomSheetDialogFragment
 import com.android.example.tanmen.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    var latitude = 0.0
+    var longitude = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fetchLocation()
     }
+
+    fun fetchLocation() {
+        val task = fusedLocationClient.lastLocation
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            return
+        }
+        task.addOnSuccessListener {
+            if (it != null) {
+                latitude = it.latitude
+                longitude = it.longitude
+
+                Log.d("location", "${latitude} ${longitude}")
+            }
+        }
+    }
+
 }
