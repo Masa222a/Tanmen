5a626452ac3cf5d64f8c06a1bdf1daaff53149c3
Masa222a <guitarstring0716@gmail.com>
2022-08-20 13:39:25 +0900

    戻るボタンを押した際にDetailFragmentを閉じる

--- app/src/main/java/com/android/example/tanmen/Controller/Fragment/DetailFragment.kt
+++ app/src/main/java/com/android/example/tanmen/Controller/Fragment/DetailFragment.kt
@@ -1,19 +1,31 @@
 package com.android.example.tanmen.Controller.Fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.android.example.tanmen.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailBinding
+    @SuppressLint("FragmentBackPressedCallback")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDetailBinding.inflate(inflater, container, false)
+
+        requireActivity().onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                parentFragmentManager
+                    .beginTransaction()
+                    .remove(this@DetailFragment)
+                    .commit()
+            }
+        })
 
         return binding.root
     }
