fcaeb7d25f16170e510f8a107d87e692f650e173
Masa222a <guitarstring0716@gmail.com>
2022-09-30 12:29:33 +0900

    詳細画面へ遷移

--- app/src/main/java/com/android/example/tanmen/Controller/Fragment/DetailFragment.kt
+++ app/src/main/java/com/android/example/tanmen/Controller/Fragment/DetailFragment.kt
@@ -8,7 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import com.android.example.tanmen.Model.Shop
 import com.android.example.tanmen.databinding.FragmentDetailBinding
+import com.squareup.picasso.Picasso
 
 class DetailFragment : Fragment() {
     private lateinit var binding: FragmentDetailBinding
@@ -19,8 +21,14 @@ class DetailFragment : Fragment() {
     ): View? {
         binding = FragmentDetailBinding.inflate(inflater, container, false)
 
-        val args = arguments?.getSerializable("shopDetail")
-        Log.d("detail", "$args")
+        val args = arguments?.getSerializable("shopDetail") as Shop
+        if (args != null) {
+            Picasso.get().load(args.image).resize(300, 300).into(binding.shopPhoto)
+            binding.shopName.text = args.name
+            binding.shopAddress.text = args.address
+            binding.shopBusinessHours.text = args.hours
+        }
+        Log.d("detailshop", "$args")
 
         requireActivity().onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
