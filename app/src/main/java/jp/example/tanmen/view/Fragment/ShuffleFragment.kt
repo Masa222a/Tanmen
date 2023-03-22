7ca6ab3225c43ca1119df213d25e89c90918d172
Masa222a <guitarstring0716@gmail.com>
2023-03-22 23:20:25 +0900

    取得したデータをvalueに入れる

--- app/src/main/java/jp/example/tanmen/view/Fragment/ShuffleFragment.kt
+++ app/src/main/java/jp/example/tanmen/view/Fragment/ShuffleFragment.kt
@@ -1,5 +1,6 @@
 package jp.example.tanmen.view.Fragment
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.ProgressDialog
 import android.content.DialogInterface
@@ -10,17 +11,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.squareup.picasso.Picasso
 import jp.example.tanmen.Model.API.ShopService
-import jp.example.tanmen.Model.Entity.Shop
 import jp.example.tanmen.R
-import jp.example.tanmen.viewModel.ShuffleViewModel
 import jp.example.tanmen.databinding.FragmentShuffleBinding
+import jp.example.tanmen.viewModel.ShuffleViewModel
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -41,11 +39,13 @@ class ShuffleFragment : Fragment() {
         return binding.root
     }
 
+    @OptIn(DelicateCoroutinesApi::class)
+    @SuppressLint("SuspiciousIndentation")
     override fun onResume() {
         super.onResume()
 
         val progressDialog = ProgressDialog(activity)
-        lifecycleScope.launch {
+//        lifecycleScope.launch {
             progressDialog.apply {
                 setTitle("検索中です")
                 setProgressStyle(ProgressDialog.STYLE_SPINNER)
@@ -53,41 +53,43 @@ class ShuffleFragment : Fragment() {
             }
             if (ShopService.instance.location != null) {
                 viewModel.getData()
-                val shopData: MutableLiveData<Shop>? = viewModel.data
-                if (shopData == null) {
-                    GlobalScope.launch(Dispatchers.Main) {
-                        binding.nameLabel.visibility = View.GONE
-                        binding.addressLabel.visibility = View.GONE
-                        progressDialog.dismiss()
-                        AlertDialog.Builder(requireActivity())
-                            .setMessage("該当する店舗が見つかりませんでした。\n検索条件を変更してください。")
-                            .setPositiveButton("はい", object : DialogInterface.OnClickListener {
-                                override fun onClick(dialog: DialogInterface?, which: Int) {
-                                    val mainFragment = this@ShuffleFragment.parentFragment as MainFragment
-                                    val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager) as ViewPager2
-                                    val bottomNav = activity?.findViewById<BottomNavigationView>(R.id.bottom_navigation)
-                                    viewPager.currentItem = 0
-                                    mainFragment.openBottomSheet()
-                                }
-                            })
-                            .show()
+                viewModel.data?.observe(viewLifecycleOwner) {
+                    if (it == null) {
+                        GlobalScope.launch(Dispatchers.Main) {
+                            binding.nameLabel.visibility = View.GONE
+                            binding.addressLabel.visibility = View.GONE
+                            progressDialog.dismiss()
+                            AlertDialog.Builder(requireActivity())
+                                .setMessage("該当する店舗が見つかりませんでした。\n検索条件を変更してください。")
+                                .setPositiveButton("はい", object : DialogInterface.OnClickListener {
+                                    override fun onClick(dialog: DialogInterface?, which: Int) {
+                                        val mainFragment = this@ShuffleFragment.parentFragment as MainFragment
+                                        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager) as ViewPager2
+                                        val bottomNav = activity?.findViewById<BottomNavigationView>(R.id.bottom_navigation)
+                                        viewPager.currentItem = 0
+                                        mainFragment.openBottomSheet()
+                                    }
+                                })
+                                .show()
 
-                        Log.d("ShuffleFragment", "店のdataが見つかりませんでした。")
+                            Log.d("ShuffleFragment", "店のdataが見つかりませんでした。")
+                        }
+                    } else {
+                        GlobalScope.launch(Dispatchers.Main) {
+                            binding.nameLabel.visibility = View.VISIBLE
+                            binding.addressLabel.visibility = View.VISIBLE
+                            progressDialog.dismiss()
+                            Picasso.get().load(it.image).resize(72, 72).into(binding.shopPhoto)
+                            binding.shopName.text = it.name
+                            binding.shopAddress.text = it.address
+                        }
+
+                        Log.d("ShuffleFragment", "${ShopService.instance.location}")
                     }
-                } else {
-                    progressDialog.dismiss()
-                    binding.nameLabel.visibility = View.VISIBLE
-                    binding.addressLabel.visibility = View.VISIBLE
-                    shopData.observe(viewLifecycleOwner) {
-                        Picasso.get().load(it.image).resize(72, 72).into(binding.shopPhoto)
-                        binding.shopName.text = it.name
-                        binding.shopAddress.text = it.address
-                    }
-                    Log.d("ShuffleFragment", "${ShopService.instance.location}")
                 }
             } else {
                 Log.d("ShuffleFragmentLocation", "locationがnullです。")
             }
-        }
+//        }
     }
 }
--- app/src/main/java/jp/example/tanmen/viewModel/ShuffleViewModel.kt
+++ app/src/main/java/jp/example/tanmen/viewModel/ShuffleViewModel.kt
@@ -10,8 +10,12 @@ class ShuffleViewModel : ViewModel() {
 
     fun getData() {
         ShopService.instance.fetchUrl(ShopService.UrlCreate.Distance.fiveHundred) {
-            val index = (0..it.size).random()
-            data = MutableLiveData(Shop(it[index].image, it[index].name, it[index].address, it[index].hours))
+            if(it.isEmpty()) {
+                data?.postValue(null)
+            } else {
+                val index = (0..it.size-1).random()
+                data?.postValue(Shop(it[index].image, it[index].name, it[index].address, it[index].hours))
+            }
         }
     }
 }
