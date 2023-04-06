54d43ab419b2f53597d55e30df57d84ca6f125ad
Masa222a <guitarstring0716@gmail.com>
2023-04-06 20:02:44 +0900

    起動直後にランダム表示画面へタブを切り替えるとクラッシュするのを修正

--- app/src/main/java/jp/example/tanmen/view/Fragment/ShuffleFragment.kt
+++ app/src/main/java/jp/example/tanmen/view/Fragment/ShuffleFragment.kt
@@ -1,11 +1,8 @@
 package jp.example.tanmen.view.Fragment
 
-import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.ProgressDialog
-import android.content.DialogInterface
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,12 +17,12 @@ import jp.example.tanmen.viewModel.ShuffleViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import timber.log.Timber.d
 
 class ShuffleFragment : Fragment() {
     private lateinit var binding: FragmentShuffleBinding
     private val viewModel: ShuffleViewModel by viewModels()
+    val progressDialog: ProgressDialog? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,8 +35,7 @@ class ShuffleFragment : Fragment() {
             shuffleViewModel = viewModel
         }
 
-        val progressDialog = ProgressDialog(activity)
-        progressDialog.apply {
+        progressDialog?.apply {
             setTitle(getString(R.string.searching))
             setProgressStyle(ProgressDialog.STYLE_SPINNER)
             show()
@@ -47,57 +43,63 @@ class ShuffleFragment : Fragment() {
 
         if(ShopService.instance.location != null) {
             viewModel.getData()
-
-            viewModel.data.observe(viewLifecycleOwner) {
-                if (it == null) {
-                    d("オブザーブ内null")
-                    GlobalScope.launch(Dispatchers.Main) {
-                        progressDialog.dismiss()
-                        binding.apply {
-                            shopPhoto.visibility = View.GONE
-                            nameLabel.visibility = View.GONE
-                            shopName.visibility = View.GONE
-                            addressLabel.visibility = View.GONE
-                            shopAddress.visibility = View.GONE
-                        }
-                        AlertDialog.Builder(requireActivity())
-                            .setMessage(getString(R.string.no_search_result))
-                            .setPositiveButton(getString(R.string.yes)) { _, _ ->
-                                val mainFragment = this@ShuffleFragment.parentFragment as MainFragment
-                                val viewPager = activity?.findViewById(R.id.viewPager) as ViewPager2
-                                viewPager.currentItem = 0
-                                mainFragment.openBottomSheet()
-                            }
-                            .show()
-                        d("店のdataが見つかりませんでした")
-                    }
-                } else {
-                    d("data observe")
-                    GlobalScope.launch(Dispatchers.Main) {
-                        binding.apply {
-                            shopPhoto.visibility = View.VISIBLE
-                            nameLabel.visibility = View.VISIBLE
-                            shopName.visibility = View.VISIBLE
-                            addressLabel.visibility = View.VISIBLE
-                            shopAddress.visibility = View.VISIBLE
-                            Picasso.get().load(it.image).resize(72, 72).into(shopPhoto)
-                            shopName.text = it.name
-                            shopAddress.text = it.address
-                        }
-                    }
-                    progressDialog.dismiss()
-                }
-            }
         } else {
-            progressDialog.dismiss()
+            progressDialog?.dismiss()
             d("locationがnullです")
         }
+
+        setupObserve()
 
         return binding.root
     }
     
     override fun onResume() {
         super.onResume()
-        viewModel.getData()
+        if (ShopService.instance.location != null) {
+            viewModel.getData()
+        }
+    }
+
+    private fun setupObserve() {
+        viewModel.data.observe(viewLifecycleOwner) {
+            if (it == null) {
+                d("オブザーブ内null")
+                GlobalScope.launch(Dispatchers.Main) {
+                    progressDialog?.dismiss()
+                    binding.apply {
+                        shopPhoto.visibility = View.GONE
+                        nameLabel.visibility = View.GONE
+                        shopName.visibility = View.GONE
+                        addressLabel.visibility = View.GONE
+                        shopAddress.visibility = View.GONE
+                    }
+                    AlertDialog.Builder(requireActivity())
+                        .setMessage(getString(R.string.no_search_result))
+                        .setPositiveButton(getString(R.string.yes)) { _, _ ->
+                            val mainFragment = this@ShuffleFragment.parentFragment as MainFragment
+                            val viewPager = activity?.findViewById(R.id.viewPager) as ViewPager2
+                            viewPager.currentItem = 0
+                            mainFragment.openBottomSheet()
+                        }
+                        .show()
+                    d("店のdataが見つかりませんでした")
+                }
+            } else {
+                d("data observe")
+                GlobalScope.launch(Dispatchers.Main) {
+                    binding.apply {
+                        shopPhoto.visibility = View.VISIBLE
+                        nameLabel.visibility = View.VISIBLE
+                        shopName.visibility = View.VISIBLE
+                        addressLabel.visibility = View.VISIBLE
+                        shopAddress.visibility = View.VISIBLE
+                        Picasso.get().load(it.image).resize(72, 72).into(shopPhoto)
+                        shopName.text = it.name
+                        shopAddress.text = it.address
+                    }
+                }
+                progressDialog?.dismiss()
+            }
+        }
     }
 }
